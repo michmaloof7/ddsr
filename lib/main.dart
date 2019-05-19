06dc31e71a8477596af410a88e11627f076fd692
Juan Manrique <juanfmf2209@hotmail.com>
2019-05-19 15:48:03 -0500

    Added all the changes made for flutter
    - On the revert merge, the changes were taken out,
    added everything from the lib folder in the drawer name branch into master.
    - As of now master has all the changes made up until this point.
    - From this point onwards will delete all the other branchs to start anew

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ddsr/views/home.dart';
+import 'package:ddsr/views/archive.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,84 +9,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
       title: 'appTitle',
       initialRoute: '/',
       routes: {
         '/': (context) => MyHome(),
-        '/second': (context) => SecondPage()
+        '/archive': (context) => ArchivePage()
       },
     );
   }
 }
 
-class BaseDrawer extends StatelessWidget{
-  String getCurrentRouteName(context) {
-    String currentRouteName;
 
-    Navigator.popUntil(context, (route) {
-      currentRouteName = route.settings.name;
-      return true;
-    });
-
-    return currentRouteName;
-  }
-  @override
-  Widget build(BuildContext context){
-    String currentRoute = getCurrentRouteName(context);
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          ListTile(
-            title: Text('Item 1'),
-            onTap: (){
-              if(currentRoute == '/') return;
-              Navigator.pop(context);
-              Navigator.of(context).pushNamed('/');
-            }
-          ),
-          ListTile(
-            title: Text('Item 2'),
-            onTap: (){
-              if(currentRoute == '/second') return;
-              Navigator.pop(context);
-              Navigator.of(context).pushNamed('/second');
-            }
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-String getCurrentRouteName(context) {
-  String currentRouteName;
-
-  Navigator.popUntil(context, (route) {
-    currentRouteName = route.settings.name;
-    return true;
-  });
-  return currentRouteName;
-}
-
-class MyHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: new Text('asdf')),
-      body: Text('first page'),
-      drawer: BaseDrawer(),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:  new Text('asdf')),
-      body: Text('second page'),
-      drawer: BaseDrawer(),
-    );
-  }
-}

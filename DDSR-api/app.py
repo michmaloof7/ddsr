9f8a8874f79814d4879377fda9513c604010d73a
JuanFelipe <jmanrique@decodeit.co>
2019-05-23 16:09:52 -0500

    Changes to docker
    - Docker-compose now creates a volume on building, in said volume it will save all the registers for the database, which will keep them alive when the containers turn off.
    - Defined volumes for the api folder, when editing the code it will detect the changes inside the container and rebuild accordingly, THAT DOESNT MEAN YOU CAN EXPLODE THE APP, IF THE APP EXPLODES YOU WILL NEED TO EXECUTE IT MANUALLY OR STOP AND RESTART THE CONTAINER.

--- DDSR-api/app.py
+++ DDSR-api/app.py
@@ -12,5 +12,5 @@ CORS(app, resources=r'/mongo/*')
 app.register_blueprint(api_v1)
 
 if __name__=='__main__':
-    app.run(debug=True, port=5000,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=5000)
 

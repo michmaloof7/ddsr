f376254fb46439f448ba49f31354a5d2b7a03bfa
Juan Manrique <juanfmf2209@hotmail.com>
2019-05-29 03:33:12 -0500

    Show details of each food
    - Show the detailed information of each food in the database

--- lib/views/foodmenu.dart
+++ lib/views/foodmenu.dart
@@ -28,48 +28,164 @@ class FoodList extends StatefulWidget {
 
 class _FoodListState extends State<FoodList> {
   List<Food> foodlist;
+  int _activeMeterIndex;
+  String msg;
+  Future getfood;
+
+  //only call the api once to get all the foods
+  @override
+  void initState() {
+    super.initState();
+    getfood = getAllFoods();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new FutureBuilder(
-      future: getAllFoods(),
+      future: getfood,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
+        //if we get data on the response then we will show it
         if (snapshot.hasData) {
           foodlist = snapshot.data;
           return Container(
+            //create list with each food item
             child: new ListView.builder(
+              //shrinkWrap: true,
               itemCount: foodlist.length,
               itemBuilder: (BuildContext context, int position) {
                 final fooditem = foodlist[position];
                 return Card(
-                  child: new InkWell(
-                    onTap: () {
-                      print("tapped");
+                  //each food item can be expanded to see its details
+                  child: new ExpansionPanelList(
+                    expansionCallback: (int index, bool status) {
+                      //know which fooditems are expanded or not
+                      setState(() {
+                        _activeMeterIndex = _activeMeterIndex == position ? null : position;
+                      });
                     },
-                    child: new Column(
-                    children: <Widget>[
-                      new Padding(
-                        padding: new EdgeInsets.all(7.0),
-                        child: new Row(
-                          children: <Widget>[
-                            new Padding(
-                              padding: new EdgeInsets.all(7.0),
-                               child: new Text(fooditem.name),
-                            )
-                          ]
+                    children: [
+                      new ExpansionPanel(
+                        //expansion cell header, where the main title shows, in this case
+                        //the food name
+                        isExpanded: _activeMeterIndex == position,
+                        headerBuilder: (BuildContext context, bool isExpanded) =>
+                        new Container(
+                          padding: const EdgeInsets.only(left:15.0),
+                          alignment: Alignment.centerLeft,
+                          child: new Text(fooditem.name)
+                        ),
+                        //All the food details
+                        body: new Container(
+                          padding: const EdgeInsets.only(left:15.0),
+                          alignment: Alignment.centerLeft,
+                          child: new Column(
+                            children: <Widget>[
+                              //the row for the food price
+                              new Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text('Precio: ')
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text(fooditem.cost.toString())
+                                  ),
+                                ]
+                              ),
+                              //the row for the food type
+                              new Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text('Tipo de comida: ')
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text(msg = fooditem.cost == 1 ? "Salado" : "Postre")
+                                  ),
+                                ]
+                              ),
+                              new Container(
+                                padding: const EdgeInsets.only(left:15.0),
+                                alignment: Alignment.centerLeft,
+                                child: //we need to iterate over the ingredients
+                                new Column(
+                                  children: <Widget>[
+                                    new Text("Ingredientes: "),
+                                    new ListView.builder(
+                                      shrinkWrap: true,
+                                      itemCount: fooditem.ingredients.length,
+                                      itemBuilder: (BuildContext context, int i) {
+                                        final ingredient = fooditem.ingredients[i];
+                                        return new Column(
+                                            children: <Widget>[
+                                            new Row(
+                                              children: <Widget>[
+                                                Padding(
+                                                  padding: EdgeInsets.only(left:15, top: 10),
+                                                  child: Text("Ingrediente: ")
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(left:15, top: 10),
+                                                  child: Text(ingredient.name)
+                                                )
+                                              ]
+                                            ),
+                                            new Row(
+                                            children: <Widget>[
+                                                Padding(
+                                                  padding: EdgeInsets.only(left:15, top: 10),
+                                                  child: Text("Cantidad: ")
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(left:15, top: 10),
+                                                  child: Text(ingredient.quantity.toString())
+                                                )
+                                              ]
+                                            ),
+                                            new Row(
+                                            children: <Widget>[
+                                                Padding(
+                                                  padding: EdgeInsets.only(left:15, top: 10, bottom: 10),
+                                                  child: Text("Tipo de unidad: ")
+                                                ),
+                                                Padding(
+                                                  padding: EdgeInsets.only(left:15, top: 10, bottom: 10),
+                                                  child: Text(ingredient.unit)
+                                                )
+                                              ]
+                                            ),
+                                            new Row(
+                                              children: <Widget>[
+                                                Padding(
+                                                  padding: EdgeInsets.all(20),
+                                                  child: Text("------------------")
+                                                )
+                                              ]
+                                            ) 
+                                          ]
+                                        );
+                                      }
+                                    )
+                                  ]
+                                )
+                              )
+                            ]
+                          )
                         )
                       )
-                    ],
-                  ),
+                    ]
                   )
                 );
               }
             )
           );
         } else {
+          //show a circle progress indicator if no data
           return new CircularProgressIndicator();
         }
-      },
+      }
     );
   }
 }
